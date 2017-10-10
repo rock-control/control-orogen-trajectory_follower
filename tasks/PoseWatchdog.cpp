b51953e9d898a59760d471827a56a3729220ed93
Arne Böckmann <arne.boeckmann@dfki.de>
2017-10-10 13:46:06 +0200

    adapt to changed map interface

--- tasks/PoseWatchdog.cpp
+++ tasks/PoseWatchdog.cpp
@@ -2,6 +2,8 @@
 
 #include "PoseWatchdog.hpp"
 #include <base-logging/Logging.hpp>
+#include <maps/grid/TraversabilityMap3d.hpp>
+#include <ugv_nav4d/TravGenNode.hpp>
 
 using namespace trajectory_follower;
 
@@ -45,16 +47,23 @@ bool PoseWatchdog::startHook()
 }
 void PoseWatchdog::updateHook()
 {
+    
+    //FIXME why is currentTrajectory a vector? Shouldn't it be just one SubTrajectory?
+    
     switch(state())
     {
         case RUNNING:
             //first call to updateHook() after startHook was executed
             state(WAITING_FOR_DATA);
         case WAITING_FOR_DATA:
-                gotTraj |= _currentTrajectory.readNewest(currentTrajectory, false) == RTT::NewData;
-                gotPose |= _robot_pose.readNewest(pose, false) == RTT::NewData;
-                gotMap |= _tr_map.readNewest(map, false) == RTT::NewData;
-                std::cout << "waiting... traj:" << gotTraj << ", pose:" << gotPose << ", map:" << gotMap << std::endl;
+            gotTraj |= _currentTrajectory.readNewest(currentTrajectory, false) == RTT::NewData;
+            gotPose |= _robot_pose.readNewest(pose, false) == RTT::NewData;
+            gotMap |= _tr_map.readNewest(map, false) == RTT::NewData;
+            
+            if(currentTrajectory.empty())
+                gotTraj = false;
+            
+            std::cout << "waiting... traj:" << gotTraj << ", pose:" << gotPose << ", map:" << gotMap << std::endl;
             if(gotTraj && gotMap && gotPose)
             {
                 std::cout << "Got data, starting to watch for pose error" << std::endl;
@@ -65,7 +74,8 @@ void PoseWatchdog::updateHook()
             std::cout << "watching..." << std::endl;
             //check pose whenever we get a new pose or a new map
             if(_robot_pose.readNewest(pose, false) == RTT::NewData ||
-               _tr_map.readNewest(map, false) == RTT::NewData)
+               _tr_map.readNewest(map, false) == RTT::NewData ||
+               _currentTrajectory.readNewest(currentTrajectory, false) == RTT::NewData)
             {
                 std::cout << "checking..." << std::endl;
                 if(!checkPose())
@@ -80,7 +90,8 @@ void PoseWatchdog::updateHook()
             if(_currentTrajectory.readNewest(currentTrajectory, false) == RTT::NewData)
             {
                 std::cout << "Got new trajectory. Re-enabling Watchdog." << std::endl;
-                state(WATCHING);
+                //FIXME renable state change
+//                 state(WATCHING);
             }
             
             break;
@@ -95,7 +106,38 @@ void PoseWatchdog::updateHook()
 
 bool PoseWatchdog::checkPose()
 {
-    return true;
+    //if we are not driving we cannot leave the map
+    if(currentTrajectory.empty())
+        return true;
+    
+    //FIXME this assumes that the kind is the same for all SubTrajectories in the vector.
+    switch(currentTrajectory.front().kind)
+    {
+        //while rescuing from an unstable/unsafe position leaving the map is ok.
+        case trajectory_follower::TRAJECTORY_KIND_RESCUE:
+            return true;
+        case trajectory_follower::TRAJECTORY_KIND_NORMAL:
+        {
+            const maps::grid::TraversabilityNodeBase* node = map.getData().getClosestNode(pose.position);
+            if(node)
+            {
+                const double zDist = fabs(node->getHeight() - pose.position.z());
+                if(zDist <= _stepHeight.value())
+                {
+                    return true;
+                }
+                std::cout << "checkPose: node too far away" << std::endl;
+                return false;
+            }
+            std::cout << "checkPose: no node at position" << std::endl;
+            return false;
+        }
+            break;
+        default:
+            throw std::runtime_error("unknown trajectory kind: " + currentTrajectory.front().kind);
+    }
+    std::cout << "checkPose failed" << std::endl;
+    return false;
 }
 
 
