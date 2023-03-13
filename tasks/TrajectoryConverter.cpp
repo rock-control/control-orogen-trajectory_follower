124d16bf1a90f7f3f83b4fff15d3e38ea32e2483
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2023-03-13 11:58:20 +0100

    Fixed indentation.

--- tasks/TrajectoryConverter.cpp
+++ tasks/TrajectoryConverter.cpp
@@ -18,46 +18,46 @@ void TrajectoryConverter::updateHook()
     // Read list of poses
     std::vector<base::Pose> input;
     if(_poses.readNewest(input, false) != RTT::NewData)
-	      return;
-	 
-	// Create the trajectory 
+        return;
+
+    // Create the trajectory 
     std::vector<trajectory_follower::SubTrajectory> output;
     base::Position prev_position;
-	base::Vector3d prev_tangent;
-	bool first = true;
+    base::Vector3d prev_tangent;
+    bool first = true;
 
-	for(base::Pose pose : input)
-	{
+    for(base::Pose pose : input)
+    {
         base::Vector3d tangent = pose.position + (pose.orientation * Eigen::Vector3d::UnitX());
-		if(first)
-		{
-			first = false;
-		}else
-		{
-			std::vector<base::Position> positions;
-			std::vector<base::geometry::SplineBase::CoordinateType> types;
-			positions.push_back(prev_position);
-			positions.push_back(prev_tangent);
-			types.push_back(base::geometry::SplineBase::ORDINARY_POINT);
-			types.push_back(base::geometry::SplineBase::TANGENT_POINT_FOR_PRIOR);
-			
-			positions.push_back(pose.position);
-			types.push_back(base::geometry::SplineBase::ORDINARY_POINT);
-			positions.push_back(tangent);
-			types.push_back(base::geometry::SplineBase::TANGENT_POINT_FOR_PRIOR);
+        if(first)
+        {
+            first = false;
+        }else
+        {
+            std::vector<base::Position> positions;
+            std::vector<base::geometry::SplineBase::CoordinateType> types;
+            positions.push_back(prev_position);
+            positions.push_back(prev_tangent);
+            types.push_back(base::geometry::SplineBase::ORDINARY_POINT);
+            types.push_back(base::geometry::SplineBase::TANGENT_POINT_FOR_PRIOR);
 
-			// Create SubTrajectory
-			base::Trajectory trajectory;
-			trajectory.spline.interpolate(positions, std::vector<double>(), types);
-			trajectory_follower::SubTrajectory sub(trajectory);
-			sub.driveMode = trajectory_follower::DriveMode::ModeAckermann;
-			sub.speed = _velocity;
-			output.push_back(sub);
-		}
+            positions.push_back(pose.position);
+            types.push_back(base::geometry::SplineBase::ORDINARY_POINT);
+            positions.push_back(tangent);
+            types.push_back(base::geometry::SplineBase::TANGENT_POINT_FOR_PRIOR);
 
-		prev_position = pose.position;
-		prev_tangent = tangent;
-	}
-	_trajectory.write(output);
+            // Create SubTrajectory
+            base::Trajectory trajectory;
+            trajectory.spline.interpolate(positions, std::vector<double>(), types);
+            trajectory_follower::SubTrajectory sub(trajectory);
+            sub.driveMode = trajectory_follower::DriveMode::ModeAckermann;
+            sub.speed = _velocity;
+            output.push_back(sub);
+        }
+
+        prev_position = pose.position;
+        prev_tangent = tangent;
+    }
+    _trajectory.write(output);
 }
 
